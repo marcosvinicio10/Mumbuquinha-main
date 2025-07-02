562e29cae10ee4345a150648ea21918664370d66
thek8bit <thiagoribeiro8bt@gmail.com>
2025-07-02 13:33:44 -0300

    02/07 - 13:33

---
+++ Assets/Scripts/SelectedCharacter.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SelectedCharacter : MonoBehaviour
+{
+    public void TainaSelected()
+    {
+        Debug.Log("Taina Selecionada!");
+    }
+
+    public void MumbucasSelected()
+    {
+        Debug.Log("Mumbucas Selecionado");
+    }
+
+    public void CatSelecionado()
+    {
+        Debug.Log("Cat Selecionado!");
+    }
+
+
+
+}
